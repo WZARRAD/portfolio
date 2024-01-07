1900d2ff21a6f39fe1894b79644fbc5dd6f7bfc5
Walid ZARRAD <153425098+WZARRAD@users.noreply.github.com>
2024-01-07 11:42:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ toggleMenu.addEventListener('click', ()=>{
     navMenu.classList.toggle('show');
 });
 
+// Tabs
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
 // Hidden
 closeMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('show');
